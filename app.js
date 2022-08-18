6b3181022c2ab9d86edfcca5c9a746252edd1b14
AZZIE2000 <azzam.faraj0@gmail.com>
2022-08-18 17:16:18 +0300

    well its running just need to be styled

---
+++ app.js
@@ -0,0 +1,54 @@
+function Collection(name, pic, level) {
+  this.name = name;
+  this.pic = pic;
+  this.level = level;
+}
+let pokies = [];
+fetch("https://digimon-api.vercel.app/api/digimon")
+  .then((res) => res.json())
+  .then((data) => {
+    for (x = 0; x < 100; x++) {
+      let newPoki = new Collection(data[x].name, data[x].img, data[x].level);
+      pokies.push(newPoki);
+    }
+    console.log(pokies);
+    console.log(data);
+    pokies.map(render);
+  });
+console.log(pokies);
+
+let myDiv = document.getElementById("cardsPlace");
+
+function render(pokies) {
+  // create div container and set class name
+
+  let divCol = document.createElement("div");
+  divCol.className = "col-sm-3";
+  myDiv.append(divCol);
+
+  // create div container to carry card-title, card-image, card-body, and card-text
+  let card = document.createElement("div");
+  card.className = "card";
+  divCol.append(card);
+
+  let imgCard = document.createElement("img");
+  imgCard.className = "card-img-top w-100";
+  imgCard.style.width = "50px";
+  imgCard.src = pokies.pic;
+  card.append(imgCard);
+
+  let cardBody = document.createElement("div");
+  cardBody.className = "card-body";
+  card.append(cardBody);
+
+  let cardTitle = document.createElement("h5");
+  cardTitle.className = "card-title";
+  cardTitle.textContent = pokies.name;
+  cardBody.append(cardTitle);
+
+  let cardText = document.createElement("p");
+  cardText.className = "card-text";
+  cardText.setAttribute("style", "white-space: pre;");
+  cardText.textContent = pokies.level;
+  cardBody.append(cardText);
+}
